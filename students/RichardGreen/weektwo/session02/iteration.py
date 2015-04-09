3fddd76077172ff6974207c481ee6a7404c3b288
Richard Green <greener@uw.edu>
2015-04-09 16:47:47 -0700

    add updated series with correct version

--- students/RichardGreen/weektwo/session02/iteration.py
+++ students/RichardGreen/weektwo/session02/iteration.py
@@ -4,33 +4,40 @@ nums = range(25)
 
 '''Next lets make a string of characters'''
 
-s ='a b c d e f g h i j k l m n o p'
+s = 'a b c d e f g h i j k l m n o p'
 
 
 def print_second_num(s):
-'''lets print the second value of each letter in the sequence and double it'''
-	i = 0
-	if i < len(s):
-        	print 2*(s[i])
-	i = i +2
- 
-def addnumbers(n):
-''' instead of using the built in sum function lets use iteration to add the sequence together''' 
-	sum =0
-	for item in n:
-        sum += item 
-	print sum
+    '''lets print the second value of each letter in the
+    sequence and double it'''
+
+i = 0
+if i < len(s):
+    print 2 * (s[i])
+i = i + 2
+
+
+def addnumbers(x):
+    '''instead of using the built in sum function lets use iteration
+    to add the sequence of numbers together'''
+sum = 0
+  for item in x:
+      sum += item
+  print sum
 
 
 def lookingforabc(s):
-'''Lets display our sequence until we reach a user defined character. if we dont find it exit'''
-   if item in s == 'abc':
-   print TRUE
-   else   
-   print FALSE 
+    '''Lets display our sequence until we reach a user defined character.
+    if we dont find it exit'''
 
+if item in s == 'abc':
+    return True
+else:
+    return False
 
-
-if __name__ == '__main__':
-    # Do something interesting here
+# if __name__ == '__main__':
+#     # Do something interesting here
     # It will only happen when the module is run
+    # print_second_num(nums)
+    # add_numbers(nums)
+    # looking_for_abc(s)
